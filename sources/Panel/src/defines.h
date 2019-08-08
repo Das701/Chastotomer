03b17c58c8a52f3d7efd9ecd506cad79dd792969
Sasha7b9 <Sasha7b9@tut.by>
2019-08-08 15:04:37 +0300

    Двоичные числа

--- sources/Panel/src/defines.h
+++ sources/Panel/src/defines.h
@@ -1,3 +1,22 @@
 #pragma once
 #include "mytypes.h"
 
+#define _bitset(bits)                               \
+  ((uint8)(                                         \
+  (((uint8)((uint)bits / 01)        % 010) << 0) |  \
+  (((uint8)((uint)bits / 010)       % 010) << 1) |  \
+  (((uint8)((uint)bits / 0100)      % 010) << 2) |  \
+  (((uint8)((uint)bits / 01000)     % 010) << 3) |  \
+  (((uint8)((uint)bits / 010000)    % 010) << 4) |  \
+  (((uint8)((uint)bits / 0100000)   % 010) << 5) |  \
+  (((uint8)((uint)bits / 01000000)  % 010) << 6) |  \
+  (((uint8)((uint)bits / 010000000) % 010) << 7)))
+
+
+#define BIN_U8(bits) (_bitset(0##bits))
+
+#define BIN_U16(bits1, bits0) (BIN_U8(bits1) << 8 | BIN_U8(bits0))
+
+#define BIN_U32(bits3, bits2, bits1, bits0) (BIN_U8(bits3) << 24 | BIN_U8(bits2) << 16 | BIN_U8(bits1) << 8 | BIN_U8(bits0))
+
+#define HEX_FROM_2(hex1, hex0) ((uint)(0x##hex1) << 16 | (uint)0x##hex0)
