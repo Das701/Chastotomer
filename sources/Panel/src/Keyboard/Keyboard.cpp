59fab49c7614371c526576e8f821c04b39652808
Sasha7b9 <Sasha7b9@tut.by>
2019-09-18 08:56:25 +0300

    *

--- sources/Panel/src/Keyboard/Keyboard.cpp
+++ sources/Panel/src/Keyboard/Keyboard.cpp
@@ -15,11 +15,11 @@ static TIM_HandleTypeDef handleTIM4;
 
 static Control::E controls[NUM_SL][NUM_RL] =
 {
-    {Control::GovButton,  Control::Right, Control::GovLeft,    Control::Test,      Control::A},
-    {Control::Mode,       Control::None,  Control::GovRight,   Control::None,      Control::B},
-    {Control::Indication, Control::GovRight,  Control::None,       Control::GovButton, Control::C},
-    {Control::None,       Control::None,  Control::None,       Control::Test,      Control::D},
-    {Control::Left,       Control::None,  Control::Indication, Control::Auto,      Control::None}
+    {Control::GovButton,  Control::Right,    Control::GovLeft,    Control::Test,      Control::A},
+    {Control::Mode,       Control::None,     Control::GovRight,   Control::None,      Control::B},
+    {Control::Indication, Control::GovRight, Control::None,       Control::GovButton, Control::C},
+    {Control::None,       Control::None,     Control::None,       Control::Test,      Control::D},
+    {Control::Left,       Control::None,     Control::Indication, Control::Auto,      Control::None}
 };
 
 /// Очередь сообщений - здесь все события органов управления
