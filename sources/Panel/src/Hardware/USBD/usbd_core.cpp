77478184edce8c4b9de6d07b4c1527b61922ad9a
Sasha7b9 <Sasha7b9@tut.by>
2020-11-01 10:05:46 +0300

    detrash

--- sources/Panel/src/Hardware/USBD/usbd_core.cpp
+++ sources/Panel/src/Hardware/USBD/usbd_core.cpp
@@ -29,7 +29,7 @@ USBD_StatusTypeDef USBD_Init(USBD_HandleTypeDef *pdev, USBD_DescriptorsTypeDef *
     return USBD_OK;
 }
 
-//-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
 USBD_StatusTypeDef USBD_DeInit(USBD_HandleTypeDef *pdev)
 {
     pdev->dev_state = USBD_STATE_DEFAULT;
@@ -40,7 +40,7 @@ USBD_StatusTypeDef USBD_DeInit(USBD_HandleTypeDef *pdev)
     return USBD_OK;
 }
 
-//-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
 USBD_StatusTypeDef  USBD_RegisterClass(USBD_HandleTypeDef *pdev, USBD_ClassTypeDef *pclass)
 {
     USBD_StatusTypeDef   status = USBD_OK;
@@ -58,7 +58,7 @@ USBD_StatusTypeDef  USBD_RegisterClass(USBD_HandleTypeDef *pdev, USBD_ClassTypeD
     return status;
 }
 
-//-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
 USBD_StatusTypeDef  USBD_Start  (USBD_HandleTypeDef *pdev)
 {
     USBD_LL_Start(pdev);
@@ -66,7 +66,7 @@ USBD_StatusTypeDef  USBD_Start  (USBD_HandleTypeDef *pdev)
     return USBD_OK;
 }
 
-//-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
 USBD_StatusTypeDef  USBD_Stop   (USBD_HandleTypeDef *pdev)
 {
     pdev->pClass->DeInit(pdev, (uint8)pdev->dev_config);
@@ -75,13 +75,13 @@ USBD_StatusTypeDef  USBD_Stop   (USBD_HandleTypeDef *pdev)
     return USBD_OK;
 }
 
-//-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
 USBD_StatusTypeDef  USBD_RunTestMode (USBD_HandleTypeDef  *) 
 {
     return USBD_OK;
 }
 
-//-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
 USBD_StatusTypeDef USBD_SetClassConfig(USBD_HandleTypeDef  *pdev, uint8_t cfgidx)
 {
     USBD_StatusTypeDef   ret = USBD_FAIL;
@@ -96,14 +96,14 @@ USBD_StatusTypeDef USBD_SetClassConfig(USBD_HandleTypeDef  *pdev, uint8_t cfgidx
     return ret;
 }
 
-//-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
 USBD_StatusTypeDef USBD_ClrClassConfig(USBD_HandleTypeDef  *pdev, uint8_t cfgidx)
 {
     pdev->pClass->DeInit(pdev, cfgidx);
     return USBD_OK;
 }
 
-//-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
 USBD_StatusTypeDef USBD_LL_SetupStage(USBD_HandleTypeDef *pdev, uint8_t *psetup)
 {
     USBD_ParseSetupRequest(&pdev->request, psetup);
@@ -133,7 +133,7 @@ USBD_StatusTypeDef USBD_LL_SetupStage(USBD_HandleTypeDef *pdev, uint8_t *psetup)
     return USBD_OK;
 }
 
-//-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
 USBD_StatusTypeDef USBD_LL_DataOutStage(USBD_HandleTypeDef *pdev , uint8_t epnum, uint8_t *pdata)
 {
     USBD_EndpointTypeDef    *pep;
@@ -167,7 +167,7 @@ USBD_StatusTypeDef USBD_LL_DataOutStage(USBD_HandleTypeDef *pdev , uint8_t epnum
     return USBD_OK;
 }
 
-//-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
 USBD_StatusTypeDef USBD_LL_DataInStage(USBD_HandleTypeDef *pdev ,uint8_t epnum, uint8_t *pdata)
 {
     USBD_EndpointTypeDef    *pep;
@@ -223,7 +223,7 @@ USBD_StatusTypeDef USBD_LL_DataInStage(USBD_HandleTypeDef *pdev ,uint8_t epnum, 
     return USBD_OK;
 }
 
-//-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
 USBD_StatusTypeDef USBD_LL_Reset(USBD_HandleTypeDef  *pdev)
 {
     USBD_LL_OpenEP(pdev,
@@ -249,14 +249,14 @@ USBD_StatusTypeDef USBD_LL_Reset(USBD_HandleTypeDef  *pdev)
     return USBD_OK;
 }
 
-//-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
 USBD_StatusTypeDef USBD_LL_SetSpeed(USBD_HandleTypeDef  *pdev, USBD_SpeedTypeDef speed)
 {
     pdev->dev_speed = speed;
     return USBD_OK;
 }
 
-//-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
 USBD_StatusTypeDef USBD_LL_Suspend(USBD_HandleTypeDef  *pdev)
 {
     CABLE_USB_IS_CONNECTED = false;
@@ -266,14 +266,14 @@ USBD_StatusTypeDef USBD_LL_Suspend(USBD_HandleTypeDef  *pdev)
     return USBD_OK;
 }
 
-//-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
 USBD_StatusTypeDef USBD_LL_Resume(USBD_HandleTypeDef  *pdev)
 {
     pdev->dev_state = pdev->dev_old_state;
     return USBD_OK;
 }
 
-//-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
 USBD_StatusTypeDef USBD_LL_SOF(USBD_HandleTypeDef  *pdev)
 {
     if (pdev->dev_state == USBD_STATE_CONFIGURED)
@@ -286,19 +286,19 @@ USBD_StatusTypeDef USBD_LL_SOF(USBD_HandleTypeDef  *pdev)
     return USBD_OK;
 }
 
-//-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
 USBD_StatusTypeDef USBD_LL_IsoINIncomplete(USBD_HandleTypeDef  *, uint8_t)
 {
     return USBD_OK;
 }
 
-//-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
 USBD_StatusTypeDef USBD_LL_IsoOUTIncomplete(USBD_HandleTypeDef  *, uint8_t)
 {
     return USBD_OK;
 }
 
-//-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
 USBD_StatusTypeDef USBD_LL_DevConnected(USBD_HandleTypeDef  *)
 {
     CABLE_USB_IS_CONNECTED = true;
@@ -306,7 +306,7 @@ USBD_StatusTypeDef USBD_LL_DevConnected(USBD_HandleTypeDef  *)
     return USBD_OK;
 }
 
-//-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
 USBD_StatusTypeDef USBD_LL_DevDisconnected(USBD_HandleTypeDef  *pdev)
 {
     pdev->dev_state = USBD_STATE_DEFAULT;
