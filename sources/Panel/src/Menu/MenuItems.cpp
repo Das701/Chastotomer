6881ef834e2506fc9351a7cb9bb6a89444993200
Sasha7b9 <Sasha7b9@tut.by>
2020-11-05 14:40:40 +0300

    *

--- sources/Panel/src/Menu/MenuItems.cpp
+++ sources/Panel/src/Menu/MenuItems.cpp
@@ -57,7 +57,7 @@ void Page::Draw(int x, int y, int, bool)
     {
         if (i == selectedItem)
         {
-            Rectangle(WidthItem(i), HEIGHT - 1).Fill(x + 1, y + 1, Color::_14);
+            Rectangle(WidthItem(i), HEIGHT - 1).Fill(x + 1, y + 1, Color::WHITE);
         }
         items[i]->Draw(x, y, WidthItem(i), i == selectedItem);
         Rectangle(WidthItem(i), HEIGHT).Draw(x, y, Color::WHITE);
