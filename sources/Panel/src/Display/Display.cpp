d600d16e6b5db8a66c3304f573fbff4ffb197f53
Sasha7b9 <Sasha7b9@tut.by>
2020-11-05 14:30:10 +0300

    detrash

--- sources/Panel/src/Display/Display.cpp
+++ sources/Panel/src/Display/Display.cpp
@@ -149,13 +149,6 @@ static void DrawBottomPart()
 
 static void DrawScreen()
 {
-//    for (int i = 0; i < Display::WIDTH; i++)
-//    {
-//        Color((uint8)i).SetAsCurrent();
-//        VLine(Display::HEIGHT).Draw(i, 0);
-//    }
-
-
     if(PageIndication::calibration.Is(Calibration::Pressed))
     {
         char buffer[20];
