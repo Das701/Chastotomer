e3414b7e9bec1753c30e932b87a50c4c7ea1b82e
Sasha7b9 <Sasha7b9@tut.by>
2020-11-05 11:45:17 +0300

    *

--- sources/Panel/src/Display/Display.cpp
+++ sources/Panel/src/Display/Display.cpp
@@ -66,6 +66,12 @@ static int second = 0;
 static int topRow = 0;
 
 
+static uint timeStart = 0;
+static uint timeDraw = 0;
+static uint timeSend = 0;
+static uint timeFull = 0;
+
+
 void Display::DrawWelcomeScreen()
 {   
     BeginScene();
@@ -84,9 +90,13 @@ void Display::Update()
 {
     if (needRedraw)
     {
+        timeStart = TIME_MS;
+
         DrawTopPart();
 
         DrawBottomPart();
+
+        timeFull = TIME_MS - timeStart;
     }
 
     needRedraw = false;
@@ -97,11 +107,23 @@ static void DrawTopPart()
 {
     topRow = 0;
 
+    uint timeStartDraw = TIME_MS;
+
     Display::BeginScene();
 
     DrawScreen();
 
+    Text(String("Рис %d", timeDraw).c_str()).Write(100, 50, Color::WHITE);
+    Text(String("Зас %d", timeSend).c_str()).Write(100, 70, Color::WHITE);
+    Text(String("Пол %d", timeFull).c_str()).Write(100, 90, Color::WHITE);
+
+    timeDraw = TIME_MS - timeStartDraw;
+
+    uint timeStartSend = TIME_MS;
+
     Display::EndScene();
+
+    timeSend = TIME_MS - timeStartSend;
 }
 
 
@@ -109,16 +131,30 @@ static void DrawBottomPart()
 {
     topRow = Display::HEIGHT / 2;
 
+    uint timeStartDraw = TIME_MS;
+
     Display::BeginScene();
 
     DrawScreen();
 
+    timeDraw += (TIME_MS - timeStartDraw);
+
+    uint timeStartSend = TIME_MS;
+
     Display::EndScene();
+
+    timeSend += (TIME_MS - timeStartSend);
 }
 
 
 static void DrawScreen()
 {
+    for (int i = 0; i < Display::WIDTH; i++)
+    {
+        VLine(Display::HEIGHT).Draw(i, 0, (Color)i);
+    }
+
+
     if(PageIndication::calibration.Is(Calibration::Pressed))
     {
         char buffer[20];
