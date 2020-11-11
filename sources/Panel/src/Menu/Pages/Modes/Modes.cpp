23d0594eda85746899bfe8c89d4e5ccff6a68b9f
Sasha7b9 <Sasha7b9@tut.by>
2020-11-11 14:05:29 +0300

    refact

--- sources/Panel/src/Menu/Pages/Modes/Modes.cpp
+++ sources/Panel/src/Menu/Pages/Modes/Modes.cpp
@@ -161,3 +161,143 @@ void PageModes::ResetModeCurrentMeasure()
     PageModesB::ResetModeCurrentMeasure();
     PageModesC::ResetModeCurrentMeasure();
 }
+
+
+bool ModeMeasureFrequency::IsRatio() const
+{
+    ModeMeasureFrequency &mode = Current();
+
+    return mode.IsRatioAB() || mode.IsRatioBA() ||
+        mode.IsRatioAC() || mode.IsRatioBC() ||
+        mode.IsRatioCA() || mode.IsRatioCB();
+}
+
+
+ModeMeasureFrequency &ModeMeasureFrequency::Current()
+{
+    ModeMeasureFrequency *const modes[Channel::Count] =
+    {
+        &PageModesA::modeMeasureFrequency,
+        &PageModesB::modeMeasureFrequency,
+        &PageModesC::modeMeasureFrequency,
+        &PageModesD::modeMeasureFrequency
+    };
+
+    return *modes[CURRENT_CHANNEL];
+}
+
+
+ModeMeasurePeriod &ModeMeasurePeriod::Current()
+{
+    ModeMeasurePeriod &result = PageModesA::modeMeasurePeriod;
+
+    if (CURRENT_CHANNEL_IS_B)
+    {
+        result = PageModesB::modeMeasurePeriod;
+    }
+
+    return result;
+}
+
+
+ModeMeasureDuration &ModeMeasureDuration::Current()
+{
+    ModeMeasureDuration &result = PageModesA::modeMeasureDuration;
+
+    if (CURRENT_CHANNEL_IS_B)
+    {
+        result = PageModesB::modeMeasureDuration;
+    }
+
+    return result;
+}
+
+
+PeriodTimeLabels &PeriodTimeLabels::Current()
+{
+    PeriodTimeLabels &result = PageModesA::periodTimeLabels;
+
+    switch (CURRENT_CHANNEL)
+    {
+    case Channel::A:    result = PageModesA::periodTimeLabels;  break;
+    case Channel::B:    result = PageModesB::periodTimeLabels;  break;
+    case Channel::C:    result = PageModesC::periodTimeLabels;  break;
+    case Channel::D: case Channel::Count:                       break;
+    }
+
+    return result;
+}
+
+
+int PeriodTimeLabels::ToZeros() const
+{
+    static const int zeros[Count] =
+    {
+        1000,
+        10000,
+        100000,
+        1000000,
+        10000000,
+        100000000
+    };
+
+    return zeros[value];
+}
+
+
+int TimeMeasure::ToMS() const
+{
+    static const int time[Count] =
+    {
+        1, 10, 100, 1000, 10000, 100000, 1000000
+    };
+
+    return time[value];
+}
+
+
+TimeMeasure &TimeMeasure::Current()
+{
+    if (CURRENT_CHANNEL_IS_A)
+    {
+        return PageModesA::timeMeasure;
+    }
+    else if (CURRENT_CHANNEL_IS_B)
+    {
+        return PageModesB::timeMeasure;
+    }
+    else if (CURRENT_CHANNEL_IS_C)
+    {
+        return PageModesC::timeMeasure;
+    }
+
+    return PageModesD::timeMeasure;
+}
+
+
+int NumberPeriods::ToAbs() const
+{
+    static const int abs[Count] =
+    {
+        1, 10, 100, 1000, 10000, 100000, 1000000
+    };
+
+    return abs[value];
+}
+
+
+NumberPeriods &NumberPeriods::Current()
+{
+    NumberPeriods &result = PageModesA::numberPeriods;
+
+    switch (CURRENT_CHANNEL)
+    {
+    case Channel::A:    result = PageModesA::numberPeriods; break;
+    case Channel::B:    result = PageModesB::numberPeriods; break;
+    case Channel::C:    result = PageModesC::numberPeriods; break;
+    case Channel::D:
+    case Channel::Count:                                    break;
+    }
+
+    return result;
+}
