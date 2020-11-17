fa5fa19e05a5de50cf0b36891909a58bad4fd17e
Sasha7b9 <Sasha7b9@tut.by>
2020-11-17 08:17:09 +0300

    Релизная win-версия без зависимостей

--- sources/scripts/vs_win/make.bat
+++ sources/scripts/vs_win/make.bat
@@ -9,7 +9,7 @@ rmdir "generated/Win32" /s /q
 
 cd scripts/vs_win
 
-cmake ../../VS/CMakeLists.txt -B../../generated/Win32 -G "Visual Studio 16 2019" -A Win32
+cmake ../../VS/CMakeLists.txt -B../../generated/Win32 -G "Visual Studio 16 2019" -A Win32 -DBUILD_USE_STATIC_RUNTIME=ON
 
 
 @echo %TIME%   Complete
