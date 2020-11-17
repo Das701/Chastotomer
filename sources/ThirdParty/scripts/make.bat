d2b87b262ccf1f7ff2c95080de0e9f814fd63ffd
Sasha7b9 <Sasha7b9@tut.by>
2020-11-17 08:31:04 +0300

    Теперь точно, наверное, Saver будет работать на произвольном PC

--- sources/ThirdParty/scripts/make.bat
+++ sources/ThirdParty/scripts/make.bat
@@ -1,2 +1,2 @@
 rmdir ..\wxWidgets\generated /s /q
-cmake.exe ..\wxWidgets\CMakeLists.txt -B..\wxWidgets\generated -G "Visual Studio 16 2019" -A Win32
+cmake.exe ..\wxWidgets\CMakeLists.txt -B..\wxWidgets\generated -G "Visual Studio 16 2019" -A Win32 -DwxBUILD_SHARED=ON -DwxBUILD_USE_STATIC_RUNTIME=ON
