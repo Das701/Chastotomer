e1916776477a58c2304e0695b5d4ed88a8a7d679
Sasha7b9 <Sasha7b9@tut.by>
2021-01-28 00:31:32 +0300

    *

--- sources/Panel/src/Calculate/ValueFPGA.h
+++ sources/Panel/src/Calculate/ValueFPGA.h
@@ -1,7 +1,11 @@
 #pragma once
+#include "Utils/String.h"
 
 
 struct ValueFPGA
 {
     ValueFPGA(uint counter1, uint counter2 = 0, uint counter3 = 0, uint counter4 = 0);
+
+    String value;
+    String units;
 };
