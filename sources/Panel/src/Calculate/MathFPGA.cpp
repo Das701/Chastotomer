06438c543189159c9fa9362d5b1dd53cc8c1c622
Sasha7b9 <Sasha7b9@tut.by>
2021-01-28 22:58:37 +0300

    *

--- sources/Panel/src/Calculate/MathFPGA.cpp
+++ sources/Panel/src/Calculate/MathFPGA.cpp
@@ -256,7 +256,7 @@ void MathFPGA::Measure::SetNewData(TypeData::E type, uint value1, uint value2, u
 {
     isDivZero = false;
 
-    value1 = 1;
+    value1 = 9;
 
     if (CreateValue(type, value1, value2, value3, value4, value5))
     {
