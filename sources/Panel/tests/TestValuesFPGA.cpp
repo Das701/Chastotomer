db996aa61d3d4ed6abf71b91a18d072fadd0fe39
Sasha7b9 <Sasha7b9@tut.by>
2021-01-29 08:34:33 +0300

    *

--- sources/Panel/tests/TestValuesFPGA.cpp
+++ sources/Panel/tests/TestValuesFPGA.cpp
@@ -25,83 +25,22 @@ static void Tests::ValueFrequency_Frequency()
     struct TestStruct
     {
         uint   counter;
-        String result;
+        String *result[TimeMeasure::Count];
     };
 
     // Channel::A
 
-    TestStruct arrayA_1ms[] =
-    {
-        {0,  "0 Hz"},
-        {1,  "0 Hz"},
-        {9,  "4 kHz"},
-        {10, "5 kHz"},      // ?
-        {11, "6 kHz"},      // ?
-        {99, "50 kHz"},
-        {100, "50 kHz"},    // ?
-        {101, "50 kHz"},    // ?
-        {(uint)-1}
-    };
+    //                                          1 ms     10 ms     100 ms      1 s      10 s      100 s
+    String results0[TimeMeasure::Count]   = {  "0 Hz",  "0 Hz",    "0 Hz",     "0 Hz",  "0 Hz",   "0 Hz" };     // 0
+    String results1[TimeMeasure::Count]   = {  "0 Hz",  "0,1 kHz", "0,01 kHz", "1 Hz",  "0,1 Hz", "0 Hz" };     // 1
+    String results9[TimeMeasure::Count]   = {  "4 kHz", "0,5 kHz", "0,04 kHz", "4 Hz",  "0,4 Hz", "0,05 Hz" };  // 9
 
-    TestStruct arrayA_10ms[] =
-    {
-        {0,  "0 Hz"},
-        {1,  "0,1 kHz"},
-        {9,  "0,5 kHz"},
-        {10, "0,5 kHz"},    // ?
-        {11, "0,6 kHz"},    // ?
-        {99, "5,0 kHz"},
-        {100, "5,0 kHz"},   // ?
-        {(uint)-1}
-    };
+    String results10[TimeMeasure::Count]  = {  "5 kHz", "0,5 kHz", "0,05 kHz", "5 Hz",  "0,5 Hz", "0,05 Hz" };  // 10
+    String results11[TimeMeasure::Count]  = {  "6 kHz", "0,6 kHz", "0,06 kHz", "5 Hz",  "0,6 Hz", "0,06 Hz" };  // 11
+    String results99[TimeMeasure::Count]  = { "50 kHz", "5,0 kHz", "0,49 kHz", "50 Hz", "5,0 Hz", "0,49 Hz" };  // 99
 
-    TestStruct arrayA_100ms[] =
-    {
-        {0,  "0 Hz"},
-        {1,  "0,01 kHz"},
-        {9,  "0,04 kHz"},
-        {10, "0,05 kHz"},   // ?
-        {11, "0,06 kHz"},   // ?
-        {99, "0,49 kHz"},
-        {100, "0,50 kHz"},  // ?
-        {(uint)-1}
-    };
-
-    TestStruct arrayA_1s[] =
-    {
-        {0,  "0 Hz"},
-        {1,  "1 Hz"},
-        {9,  "4 Hz"},
-        {10, "5 Hz"},   // ?
-        {11, "5 Hz"},   // ?
-        {99, "50 Hz"},
-        {100, "50 Hz"}, // ?
-        {(uint)-1}
-    };
-
-    TestStruct arrayA_10s[] =
-    {
-        {0,  "0 Hz"},
-        {1,  "0,1 Hz"},
-        {9,  "0,4 Hz"},
-        {10, "0,5 Hz"},     // ?
-        {11, "0,6 Hz"},     // ?
-        {99, "5,0 Hz"},     // ?
-        {100, "5,0 Hz"},    // ?
-        {(uint)-1}
-    };
-
-    TestStruct arrayA_100s[] =
-    {
-        {0,  "0 Hz"},
-        {1,  "0 Hz"},
-        {9,  "0,05 Hz"},
-        {10, "0,05 Hz"},    // ?
-        {11, "0,06 Hz"},    // ?
-        {99, "0,49 Hz"},    // ?
-        {100, "0,50 Hz"},   // ?
-        {(uint)-1}
-    };
+    String results100[TimeMeasure::Count] = { "50 kHz", "5,0 kHz", "0,50 kHz", "50 Hz", "5,0 Hz", "0,50 Hz" };  // 100
+    String results101[TimeMeasure::Count] = { "50 kHz" };                                                       // 101
 
     // Channel::B
 
